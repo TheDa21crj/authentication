132196e5f5635c0f7d68dedc3c518166a3c054e8
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-30 12:19:45 +0530

    Create server.js

---
+++ Backend/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { request } = require("express");
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE,OPTIONS"
+  );
+
+  next();
+});
+app.get("/test", (req, res) => {
+  res.send("Hello Server");
+});
+
+app.get("/", (req, res) => {
+  res.status(202).send("HEllo World");
+});
+
+// app.use("/api/user", require("./routes/user"));
+
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DB_URI).then(
+  app.listen(port, () => {
+    console.log("Listining on port " + port);
+    console.log("connected");
+  })
+);
