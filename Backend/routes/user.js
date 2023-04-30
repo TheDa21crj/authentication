e32502102a66d663b6f556df9b10a470d346ab67
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-30 12:24:24 +0530

    Create user.js

---
+++ Backend/routes/user.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const loginController = require("../controllers/loginController");
+const auth = require("../middleWare/auth");
+const { check, validationResult } = require("express-validator");
+
+// Public || Get Register User
+router.post(
+  "/signup",
+  [check("name", "name is Required").not().isEmpty()],
+  [check("email", "email is Required").not().isEmpty()],
+  [check("password", "password is Required").not().isEmpty()],
+  loginController.register
+);
+
+// Public || Get Login User
+router.post(
+  "/Login",
+  [check("email", "email is Required").not().isEmpty()],
+  [check("password", "password is Required").not().isEmpty()],
+  loginController.login
+);
+
+// auth
+router.use(auth);
+
+// Public || Get user Data
+router.post(
+  "/",
+  [check("email", "email is Required").not().isEmpty()],
+  userController.getUser
+);
+
+module.exports = router;
