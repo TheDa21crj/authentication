7116b51c1a4562bab51db5bc599077c457862168
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-30 13:30:18 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,13 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+// axios
 import axios from "axios";
 
 // Pages
 import Home from "./Pages/Home";
 import Error from "./Pages/Error";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 // defaults baseURL
 axios.defaults.baseURL = "http://localhost:5000/";
@@ -12,6 +17,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Login" element={<Login />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
