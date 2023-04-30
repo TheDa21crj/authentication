f1a164010b3775fecd9c8ceae39236a42df5b466
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-30 12:19:34 +0530

    Create auth.js

---
+++ Backend/middleWare/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const HttpError = require("../models/HttpError");
+
+module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+  try {
+    const token = req.headers.authorization; // Authorization: 'Bearer TOKEN'
+
+    if (!token) {
+      const error = new HttpError("Authentication failed!", 401);
+      return next(error);
+    }
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRATE);
+    res.locals.userData = { userEmail: decodedToken.userEmail };
+    next();
+  } catch (err) {
+    const error = new HttpError("Authentication failed!", 403);
+    console.log(err);
+    return next(error);
+  }
+};
